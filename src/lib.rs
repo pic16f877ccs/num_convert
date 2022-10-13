525b367af7a18843ea3cccd78ca33df6a2f22c0e
Karl Wulfert <pic16f877.ccs@emailn.de>
2022-10-13 11:06:22 +0200

    Add docs to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+//! # Traits for conversions between integer types.
+//!
+//! - The FromByAdd trait for conversion from integers 
+//! - The ToByAdd trait for conversion into integers
+//! - The TryFromByAdd trait for converting from integer values that can fail
+//! - The TryToByAdd trait for converting into integer values that can fail.
+//!
+//! When converting from a negative or positive value to a positive or negative value,
+//! the type and value are converted.
+//! For example -128_i8 -> 0_u8, 127_i8 -> 255_u8, 0_u8 -> 127_i8.
+//!
+//! ## Examples
+//!
+//! ```
+//! use num_convert::ToByAdd;
+//!
+//! fn convert_into_u16<T: ToByAdd>(min: T, max: T) -> (u16, u16) {
+//!     (min.into_u16(), max.into_u16())
+//! }
+//! assert_eq!((u16::MIN, u16::MAX), convert_into_u16(i16::MIN, i16::MAX));
+
 mod convert_from;
 mod convert_to;
 mod convert_try_from;
