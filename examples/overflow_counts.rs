9455f22b0d0ae95c67bbbae348431243ed31e1fa
Karl Wulfert <pic16f877.ccs@emailn.de>
2023-01-31 21:41:07 +0100

    Update example

--- examples/overflow_counts.rs
+++ examples/overflow_counts.rs
@@ -4,11 +4,11 @@ use num_convert::min_zero_max::Tzero;
 
 fn overflow_cnt<T, O>(num: T) -> (O, T)
     where
-        T: FromAs<O> + Copy + Shr<u32, Output = T> + Tzero,
+        T: FromAs<O> + Copy + Shr<u32, Output = T> + Tzero<T>,
         O: FromAs<T> + TryFrom<T> + Tbits,
 {
     match O::try_from(num) {
-        Ok(ok) => (ok, T::zero()),
+        Ok(ok) => (ok, T::tzero()),
         Err(_) => (O::from_as(num), num >> O::tbits())
     }
 }
