433a39370ab4e9cd5aa274734ad8e630a769edac
Karl Wulfert <pic16f877.ccs@emailn.de>
2023-02-07 19:31:46 +0100

    Update doc CastFromAs

--- src/cast_from_as.rs
+++ src/cast_from_as.rs
@@ -49,9 +49,10 @@ pub trait CastFromAs {
 }
 
 macro_rules! cast_from_as_impls {
-    ($($from_type:ty),+; $into_type:ty ) => {
+    ( $into_type:ty; $( $doc:expr, $($from_type:ty),* );* ) => {
         impl CastFromAs for $into_type {
             paste!{
+                #[doc = concat!("Converts ", stringify!($into_type), " to ", stringify!($into_type), " losslessly.")]
                 #[inline]
                 fn [<from_$into_type>](n: $into_type) -> Self {
                     n
@@ -59,26 +60,30 @@ macro_rules! cast_from_as_impls {
             }
 
             $(
-                paste!{
-                    #[inline]
-                    fn [<from_$from_type>](n: $from_type) -> Self {
-                        n as Self
+                $(
+                    paste!{
+                        #[doc = concat!("Converts ", stringify!($from_type), " to ", stringify!($into_type), " ",$doc,".")]
+                        #[inline]
+                        fn [<from_$from_type>](n: $from_type) -> Self {
+                            n as Self
+                        }
                     }
-                }
+                )*
             )*
         }
     }
 }
 
-cast_from_as_impls! { i16, i32, i64, isize, i128, u8, u16, u32, u64, usize, u128; i8 }
-cast_from_as_impls! { i8, i32, i64, isize, i128, u8, u16, u32, u64, usize, u128; i16 }
-cast_from_as_impls! { i8, i16, i64, isize, i128, u8, u16, u32, u64, usize, u128; i32 }
-cast_from_as_impls! { i8, i16, i32, isize, i128, u8, u16, u32, u64, usize, u128; i64 }
-cast_from_as_impls! { i8, i16, i32, i64, i128, u8, u16, u32, u64, usize, u128; isize }
-cast_from_as_impls! { i8, i16, i32, i64, isize, u8, u16, u32, u64, usize, u128; i128 }
-cast_from_as_impls! { i8, i16, i32, i64, isize, i128, u16, u32, u64, usize, u128; u8 }
-cast_from_as_impls! { i8, i16, i32, i64, isize, i128, u8, u32, u64, usize, u128; u16 }
-cast_from_as_impls! { i8, i16, i32, i64, isize, i128, u8, u16, u64, usize, u128; u32 }
-cast_from_as_impls! { i8, i16, i32, i64, isize, i128, u8, u16, u32, usize, u128; u64 }
-cast_from_as_impls! { i8, i16, i32, i64, isize, i128, u8, u16, u32, u64, u128; usize }
-cast_from_as_impls! { i8, i16, i32, i64, isize, i128, u8, u16, u32, u64, usize; u128 }
+cast_from_as_impls! { i8; "possible overflow", u8, i16, u16, i32, u32, i64, u64, isize, usize, i128, u128 }
+cast_from_as_impls! { i16; "losslessly", i8, u8; "possible overflow", u16, i32, u32, i64, u64, isize, usize, i128, u128 }
+cast_from_as_impls! { i32; "losslessly", i8, u8, i16, u16; "possible overflow", u32, i64, u64, isize, usize, i128, u128 }
+cast_from_as_impls! { i64; "losslessly", i8, u8, i16, u16, i32, u32, isize; "possible overflow", u64, usize, i128, u128 }
+cast_from_as_impls! { isize; "losslessly", i8, u8, i16, u16, i32, u32, i64; "possibe overflow", u64, usize, i128, u128 }
+cast_from_as_impls! { i128; "losslessly", i8, u8, u16, i16, u32, i32, u64, i64, usize, isize; "possibly overflow", u128 }
+
+cast_from_as_impls! { u8; "possible overflow", i8, u16, i16, i32, u32, i64, u64, isize, usize, i128, u128 }
+cast_from_as_impls! { u16; "losslessly", u8; "possible overflow", i8, i16, i32, u32, i64, u64, isize, usize, i128, u128 }
+cast_from_as_impls! { u32; "losslessly", u8, u16; "possible overflow", i8, i16, i32, i64, u64, isize, usize, i128, u128 }
+cast_from_as_impls! { u64; "losslessly", u8, u16, u32, usize; "possible overflow", i8, i16, i32, i64, isize, i128, u128 }
+cast_from_as_impls! { usize; "losslessly", u8, u16, u32, u64; "possible overflow", i8, i16, i32, i64, isize, i128, u128 }
+cast_from_as_impls! { u128; "losslessly", u8, u16, u32, u64, usize; "possible overflow", i8, i16, i32, i64, isize, i128 }
