2ca2c1f7b202df7e45caae417b2b4373cad12049
Karl Wulfert <pic16f877.ccs@emailn.de>
2023-03-20 21:06:03 +0100

    Fix clippy exmple

--- examples/multi_errors.rs
+++ examples/multi_errors.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), ConvertErrors> {
     let val_dig = <u8>::from_digits(321);
     let val_int_str = <u8>::try_from_int_str("333");
     let val_arr: Result<[u8; 2], TryTupToArrErr> = ("256", 225i32).try_into_arr();
-    let val_by_add = i8::try_from_by_add(255u16).ok_or::<ConvertErrors>(MultiErrors::TryFromByAddErr.into());
+    let val_by_add = i8::try_from_by_add(256u16).ok_or::<ConvertErrors>(MultiErrors::TryFromByAddErr.into());
     let tup_3 = (121u16, 203i16, 271u64);
     let tup_4 = (99u32, 2023u16, 17081975u64, 0u8);
     let tup_5 = (25u8, 99u32, 2023u16, 17081975i64, "1000999888");
@@ -18,6 +18,7 @@ fn main() -> Result<(), ConvertErrors> {
     println!("{}", val_dig.unwrap_err());
     println!("{}", val_int_str.unwrap_err());
     println!("{}", val_arr.unwrap_err());
+    println!("{}", val_by_add.unwrap_err());
     println!("{}", TryTupToArr::<u8>::try_into_arr(tup_3).unwrap_err());
     println!("{:?}", TryTupToArr::try_into_arr(tup_4)?.iter().sum::<u64>());
     println!( "{:?}", TryTupToArr::try_into_arr(tup_5)?.iter().sum::<u64>());
