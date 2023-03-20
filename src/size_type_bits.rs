24e27883a07c24af0d227a948e3c806b224aa85b
Karl Wulfert <pic16f877.ccs@emailn.de>
2023-03-20 18:30:26 +0100

    Update doc Tbits Sbits

--- src/size_type_bits.rs
+++ src/size_type_bits.rs
@@ -1,16 +1,28 @@
 /// Generic trait for define the size of the integers type in bits.
 ///
+/// # Usage
+/// Basic use of the trait.
+///
+/// ```
+/// use num_convert::Tbits;
+///
+/// assert_eq!(i8::tbits(), 8_u32);
+/// assert_eq!(u64::tbits(), 64_u32);
+/// ```
+///
 /// # Examples
-/// Usage:
 ///
 /// ```
 /// # use num_convert::Tbits;
 /// fn get_bits_from_type<T: Tbits>(value: T) -> u32 {
 ///     T::tbits()
 /// }
+///
+/// assert_eq!(get_bits_from_type(-128_i8), 8_u32);
+/// assert_eq!(get_bits_from_type(isize::MIN), 64_u32);
 /// ```
 pub trait Tbits {
-    /// Associated function returns the size of integer type in bits.
+    /// Returns the size of integer type in bits.
     fn tbits() -> u32;
 }
 
@@ -32,8 +44,17 @@ tbits_impls! { i8, u8, i16, u16, i32, u32, i64, u64, isize, usize, i128, u128 }
 
 /// The Sbit trait for define the size of integer value in bits.
 ///
+/// # Usage
+/// Basic use of the trait.
+///
+/// ```
+/// use num_convert::Sbits;
+///
+/// assert_eq!(127_i8.sbits(), 8_u32);
+/// assert_eq!(0_u64.sbits(), 64_u32);
+/// ```
+///
 /// # Examples
-/// Usage:
 ///
 /// ```
 /// # use num_convert::Sbits;
@@ -41,8 +62,8 @@ tbits_impls! { i8, u8, i16, u16, i32, u32, i64, u64, isize, usize, i128, u128 }
 ///     value.sbits()
 /// }
 ///
-/// assert_eq!(get_bits_from_value(-128_i8), 8_u32);
-/// assert_eq!(get_bits_from_value(65535_u16), 16_u32);
+/// assert_eq!(get_bits_from_value(65_535_u16), 16_u32);
+/// assert_eq!(get_bits_from_value(u128::MAX), 128_u32);
 /// ```
 pub trait Sbits {
     /// Method returns the size of integers value in bits.
